cd3a1cb6ac44b4501313cf42e2791ada83f52488
son nguyen <>
2021-09-09 20:33:12 +0700

    show popup when open notification tab, fix text

--- app/src/main/java/com/bvb/sotp/screen/transaction/NotificationAdapter.kt
+++ app/src/main/java/com/bvb/sotp/screen/transaction/NotificationAdapter.kt
@@ -45,12 +45,13 @@ class NotificationAdapter(
         val movie: MobilePushRealmModel = list[position]
 
         if (holder is ItemViewHolder) {
-            holder.bind(holder.itemView.context, movie)
+            holder.bind(holder.itemView.context, movie, sessionPending)
             println("---detail---------------" + movie.detail)
             println("----sessionPending--------------" + sessionPending)
 
             if (!TextUtils.isEmpty(movie.detail) && movie.detail.equals(sessionPending)) {
                 holder.itemView.setBackgroundColor(Color.parseColor("#FFD236"))
+                holder.content
             } else {
                 holder.itemView.setBackgroundColor(Color.WHITE)
             }
@@ -95,10 +96,16 @@ class ItemViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     }
 
     @SuppressLint("SetTextI18n")
-    fun bind(context: Context, model: MobilePushRealmModel) {
+    fun bind(context: Context, model: MobilePushRealmModel, sessionPending: String) {
         if (model.type == Constant.NOTI_TYPE_MOBILE_PUSH) {
-            tittle?.text = context.getString(R.string.mobile_push)
-            content?.text = context.getString(R.string.mobile_push_msg)
+            if (!TextUtils.isEmpty(model.detail) && model.detail.equals(sessionPending)) {
+                tittle?.text = context.getString(R.string.mobile_push)
+                content?.text = context.getString(R.string.mobile_push_msg_active)
+            } else {
+                tittle?.text = context.getString(R.string.mobile_push)
+                content?.text = context.getString(R.string.mobile_push_msg)
+            }
+
         } else if (model.type == Constant.NOTI_TYPE_INVALID_MOBILE_PUSH) {
             if (!TextUtils.isEmpty(model.detail)) {
                 tittle?.text =
